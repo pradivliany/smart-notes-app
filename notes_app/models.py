e51de5ea90c98b2f061fc45ac1ab8832a3042628
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-09-17 04:47:37 +0200

    black and isort to all files + add UserProfile model with avatar resizing + Add MEDIA_ROOT + MEDIA_URL

--- notes_app/models.py
+++ notes_app/models.py
@@ -5,7 +5,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=25, null=False, unique=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
 
 class Note(models.Model):
@@ -16,4 +16,4 @@ class Note(models.Model):
     tags = models.ManyToManyField(Tag)
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    avatar = models.ImageField(
+        default="avatars/default_avatar.jpg", upload_to="avatars/"
+    )
+    bio = models.CharField(max_length=500, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        try:
+            if self.avatar and self.avatar.path:
+                img = Image.open(self.avatar.path)
+                if img.height > 100 or img.width > 100:
+                    max_size = (100, 100)
+                    img.thumbnail(max_size)
+                    img.save(self.avatar.path)
+        except Exception as e:
+            print(f"Error manipulating avatar image: {e}")
