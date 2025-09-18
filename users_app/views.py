96a0d708840adf630af3bc2c9d47fa07f3de623a
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-09-18 02:58:17 +0200

    Add: Initial urls and placeholder views for user authentication

--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def signup_user(request):
+    return HttpResponse('Welcome to Signup page')
+
+
+def login_user(request):
+    return HttpResponse('Welcome to Login page')
+
+
+def logout_user(request):
+    return HttpResponse('Welcome to Logout page')
