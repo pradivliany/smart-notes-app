e7e493ce4a1f10651142ba06c12e9a420f804ecd
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-09-18 02:34:11 +0200

    Add root urls for notes_app and users_app

--- notes/urls.py
+++ notes/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("users_app.urls")),
+    path("notes/", include("notes_app.urls")),
 ]
