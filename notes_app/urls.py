7be98c05f63de77073009f8f1e12c24fee4f0058
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-09-19 14:58:07 +0200

    Add: app_name varibles to users_app and notes_app

--- notes_app/urls.py
+++ notes_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "notes_app"
+
 urlpatterns = [
     path("", views.note_list, name="note_list"),
     path("<int:note_id>/", views.note_detail, name="note_detail"),
--- users_app/urls.py
+++ users_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from . import views
 
-urlpatterns = [
-    path("signup/", views.signup_user, name='signup'),
-    path("login/", views.login_user, name='login'),
-    path("logout/", views.logout_user, name='logout'),
-]
+app_name = "users_app"
 
+urlpatterns = [
+    path("signup/", views.signup_user, name="signup"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+]
