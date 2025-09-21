183c398af9190370a932955dd193996789236363
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-09-21 05:07:52 +0200

    Add: user field to Note and Tag models to associate them with users

--- notes_app/models.py
+++ notes_app/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+from users_app.models import User
+
 
 class Tag(models.Model):
-    name = models.CharField(max_length=25, null=False, unique=True)
+    name = models.CharField(max_length=25, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f"{self.name}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "user"], name="unique tag for username"
+            )
+        ]
 
 
 class Note(models.Model):
@@ -14,6 +24,7 @@ class Note(models.Model):
     done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f"{self.name}"
