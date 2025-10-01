99c4ff3178d31420ef33bf97d8f63c1622d1e2b0
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-10-01 02:58:17 +0200

    FIX + ADD: Fixed custom filter extract_tags and Added it to templates

--- notes_app/templatetags/custom_filters.py
+++ notes_app/templatetags/custom_filters.py
@@ -3,8 +3,12 @@ from django import template
 register = template.Library()
 
 
-def extract_tags(note_tags):
-    return [tag.name for tag in note_tags.all() if len(tag.name) <= 10][:5]
+def extract_tags(note_tags, limit=5):
+    unique_tags = sorted(set(tag.name for tag in note_tags.all()))
+    if limit == -1:
+        return list(unique_tags)
+    else:
+        return list(name for name in unique_tags if len(name) <= 10)[:limit]
 
 
 register.filter("extract_tags", extract_tags)
