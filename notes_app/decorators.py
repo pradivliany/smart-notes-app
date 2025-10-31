449a8a8cdf62be75ef036ed3e4751563a8e4df44
pradivliany <yaroslavpradyvlianyi@gmail.com>
2025-10-31 04:43:26 +0100

    docs: add docstrings to functions

--- notes_app/decorators.py
+++ notes_app/decorators.py
@@ -1,16 +1,23 @@
 from functools import wraps
+from typing import Callable
 
 from django.contrib import messages
 from django.shortcuts import redirect
 
 
-def profile_confirmed_required(func):
+def profile_confirmed_required(func: Callable) -> Callable:
+    """
+    Ensures that the user's profile is confirmed before accessing a view.
+
+    - If confirmed: proceeds to the view.
+    - If not confirmed: shows an info message and redirects to profile page.
+    """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         request = args[0]
         if request.user.profile.is_confirmed:
-            result = func(*args, **kwargs)
-            return result
+            return func(*args, **kwargs)
         else:
             messages.info(
                 request,
--- users_app/signals.py
+++ users_app/signals.py
@@ -7,6 +7,11 @@ from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_or_update_profile(sender, instance, created, **kwargs):
+    """
+    Automatically creates or updates a Profile whenever a User is saved.
+    - On creation: creates a new Profile.
+    - On update: saves the existing Profile.
+    """
     if created:
         Profile.objects.create(user=instance)
     else:
